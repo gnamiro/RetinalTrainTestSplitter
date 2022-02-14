8114157e781ff80626e9acd2317fb315176257dc
naeim ghahramanpour <naeimghahramanpour@gmail.com>
2022-02-14 17:42:11 +0330

    Set threshold in amount of images, included in dataset

--- TrainTestSplitter.py
+++ TrainTestSplitter.py
@@ -17,43 +17,71 @@ def get_args():
 
   return parser.parse_args()
 
+def copy_good_samples(good_images):
+  data = list(zip(*[iter(good_images)]*1))
+  images_df = pd.DataFrame(data, columns=['healthy_fundus'])
+
+  number_of_files = len(good_images)
+  print(images_df.head())
+  msk = np.random.rand(number_of_files) < args.test_size
+  train_files, test_files = images_df[~msk], images_df[msk]
+
+  print(len(train_files))
+  num = 0
+  for index, _file in train_files.iterrows():
+    # print(index)
+    num += 1
+    shutil.copy2(f'{good_fundus_images}/{_file.healthy_fundus}', f'{train_path}')
+    if(num > 230):
+      break
+
+  num = 0
+  for index, _file in test_files.iterrows():
+    # print(index)
+    num += 1
+    shutil.copy2(f'{good_fundus_images}/{_file.healthy_fundus}', f'{test_path}/good')  
+    if(num > 30):
+      break
+
+def copy_disease_samples(disease_images):
+  data = list(zip(*[iter(disease_images)]*1))
+  images_df = pd.DataFrame(data, columns=['unhealthy_fundus'])
+
+  number_of_files = len(good_images)
+  print(images_df.head())
+  num = 0
+  for index, _file in images_df.iterrows():
+    # print(index)
+    num += 1
+    shutil.copy2(f'{disease_fundus_images}/{_file.unhealthy_fundus}', f'{test_path}/disease')
+    if(num > 100):
+      break
 
 if __name__ == '__main__':
   args = get_args()
 
+  np.random.seed(args.random_state)
   good_fundus_images = './Dataset/good'
+  disease_fundus_images = './Dataset/disease'
   
   train_path = f"{args.output_path}/train/good"
   test_path = f'{args.output_path}/test'
 
   os.makedirs(train_path, exist_ok=True)
   os.makedirs(f"{test_path}/good", exist_ok=True)
+  os.makedirs(f"{test_path}/disease", exist_ok=True)
   # os.makedirs(f"{test_path}/disease", exist_ok=True)
   # os.makedirs(f"{test_path}/ambiguous", exist_ok=True)
 
 
-  copy_tree(f'{args.images_path}/disease', test_path + '/disease')
-  copy_tree(f'{args.images_path}/ambiguous', test_path + '/ambiguous')
+  # copy_tree(f'{args.images_path}/disease', test_path + '/disease')
+  # copy_tree(f'{args.images_path}/ambiguous', test_path + '/ambiguous')
 
   good_images = [name for name in os.listdir(good_fundus_images)]
   print(len(good_images))
-  data = list(zip(*[iter(good_images)]*1))
-  images_df = pd.DataFrame(data, columns=['healthy_fundus'])
+  copy_good_samples(good_images)
 
-  number_of_files = len(good_images)
-  print(images_df.head())
-
-  np.random.seed(args.random_state)
-  msk = np.random.rand(number_of_files) < args.test_size
-  train_files, test_files = images_df[~msk], images_df[msk]
-
-  print(len(train_files))
-
-  for index, _file in train_files.iterrows():
-    # print(index)
-    shutil.copy2(f'{good_fundus_images}/{_file.healthy_fundus}', f'{train_path}')
-
-  for index, _file in test_files.iterrows():
-    # print(index)
-    shutil.copy2(f'{good_fundus_images}/{_file.healthy_fundus}', f'{test_path}/good')    
+  disease_images = [name for name in os.listdir(disease_fundus_images)]
+  print(len(disease_images))
+  copy_disease_samples(disease_images)
 
